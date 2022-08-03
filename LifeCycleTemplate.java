dad5af7309b12dcb46bbb2c2c1a337d0cf3be752
will-maruis <108104299+will-maruis@users.noreply.github.com>
2022-08-03 21:26:50 +0200

    Add files via upload

---
+++ LifeCycleTemplate.java
@@ -0,0 +1,29 @@
+package plm.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class LifeCycleTemplate {
+	Attributs ??
+	List <LifeCycleState>  lifeCycleState;
+	public String getInitialState() {
+		//
+		// Implementation and returned value are not relevant for this exercise
+		//
+		return null;
+	}
+	
+	public boolean isKnown(String lifeCycleState) {
+		//
+		// Implementation and returned value are not relevant for this exercise
+		//
+		return true;
+	}
+	
+	public boolean isFinal(String lifeCycleState) {
+		//
+		// Implementation and returned value are not relevant for this exercise
+		//
+		return true;
+	}
+}
---
+++ VersionSchema.java
@@ -0,0 +1,17 @@
+package plm.model;
+
+import javax.persistence.Entity;
+
+@Entity
+public class VersionSchema {
+	// ??  existe pas d'attributs
+	// quelles sont les attributs sur lesquelles la méthode va s'implmenter. 
+	// on pourrait définir une collection d'objets version :  List<Version>
+	
+	public String getNextVersionLabel(String currentVersionLabel) {
+		//
+		// Implementation and returned value are not relevant for this exercise
+		// ietération : interface iteratorList, qui va nous permettte grace à ses méthodes  
+		return null;
+	}
+}
